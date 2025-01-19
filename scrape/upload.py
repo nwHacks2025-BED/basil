98e0343a8ccc5b3fed60e5b949ef09427c33dd9c
davenfroberg <davenfroberg@gmail.com>
2025-01-18 16:10:23 -0800

    added code to add docs to mongo

---
+++ scrape/upload.py
@@ -0,0 +1,15 @@
+import pymongo
+import json
+from pymongo import MongoClient, InsertOne
+
+client = pymongo.MongoClient("mongodb+srv://davenfroberg:WjxywruYe42mXrVe@nwhacks.dtnoj.mongodb.net/?retryWrites=true&w=majority&appName=nwhacks")
+db = client.jobs
+collection = db.unlabelled
+
+with open('../allJobs.json') as file:
+    all_jobs = json.load(file)
+
+requests = [InsertOne(job) for job in all_jobs]
+result = collection.bulk_write(requests)
+
+print(f'Inserted {result.inserted_count} documents')
